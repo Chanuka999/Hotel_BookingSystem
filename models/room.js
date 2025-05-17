10c04ea2b87f35c5091116b619ebd5317affd39a
Chanuka999 <chanukaranditha99@gmail.com>
2025-05-17 16:36:59 +0530

    update

---
+++ models/room.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const roomScheema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    maxcount: {
+      type: Number,
+      required: true,
+    },
+    phonenumber: {
+      type: Number,
+      required: true,
+    },
+    rentperday: {
+      type: Number,
+      required: true,
+    },
+    imageurls: [],
+    currentbookings: [],
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const roomModel = mongoose.model("rooms", roomScheema);
+
+module.exports = roomModel;
