3a0cb5db00962a4f924c84af6ebd900291ad3542
Chanuka999 <chanukaranditha99@gmail.com>
2025-05-20 00:37:56 +0530

    update code

--- client/src/screens/Bookingscreen.js
+++ client/src/screens/Bookingscreen.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-const Bookingscreen = () => {
+const Bookingscreen = ({ match }) => {
+  const { roomid } = useParams();
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+  const [room, setRoom] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.post("/api/rooms/getroombyid", {
+          roomid: match.param.roomid,
+        });
+        console.log("Rooms fetched:", response.data);
+        setRoom(response.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true); // maybe this should be setError(true)?
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Booking scheem</h1>
+      <h1>Room id - {roomid}</h1>
     </div>
   );
 };
