ccc029a5acb75f4a5ff4320f0a9865e2c47b46ba
Chanuka999 <chanukaranditha99@gmail.com>
2025-05-20 08:43:19 +0530

    change code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homescreen from "./screens/Homescreen";
 import Bookingscreen from "./screens/Bookingscreen";
 
